b05f08c679ab8ddb471f1e007db09fb682dbeb01
LunaLu <nadeyavavilova1906@gmail.com>
2025-12-23 14:36:44 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,24 @@
--- Code
+# Identify the top 5 most revenue-generating products 
+  
+  -- I want to see if the product was sold more than once:
+  Select ProductID
+  FROM SalesOrderDetail;
+  -- Result: 542 rows returned, and next group all products
+
+ Select ProductID
+ FROM SalesOrderDetail
+ GROUP BY ProductID;
+ -- Result: 142 rows returned, I can see now that different orders might have the same item.
+
+ -- Group product, round amount, and sum amount for a list of 5 products, and change the column names.
+SELECT 
+    p.Name AS ProductName, 
+	  p.ProductNumber AS SKU,
+    ROUND(SUM(sod.LineTotal), 2) AS TotalRevenue
+FROM Product AS p
+JOIN SalesOrderDetail AS sod ON p.ProductID = sod.ProductID
+GROUP BY p.Name
+ORDER BY TotalRevenue DESC
+LIMIT 5;
+
+![Top_Products Preview](top_products.png)
